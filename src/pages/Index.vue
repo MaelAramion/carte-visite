e4ad6ad877e9412d552c314dceec9e263d53d5ad
Maël Aramion <mael.aramion@skazy.nc>
2022-09-26 10:03:17 +1100

    modif style

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,10 +6,20 @@
         <div class="img"></div>
         <p>Licence professionnelle MIAW, Web Developper</p>
         <div class="link-container">
-          <a href="mailto:mael.aramio@gmail.com">
-            <i class="bi bi-envelope"></i>
-            mael.aramion@gmail.com
-          </a>
+          <ul>
+            <li>
+              <a href="mailto:mael.aramio@gmail.com">
+                <i class="bi bi-envelope"></i>
+                mael.aramion@gmail.com
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/MaelAramion/carte-visite.git" target="_blank">
+                <i class="bi bi-github"></i>
+                Projet
+              </a>
+            </li>
+          </ul>
         </div>
     </div>
   </Layout>
@@ -33,7 +43,7 @@ export default {
 <style>
 @font-face {
   font-family: 'font1';
-  src: url("/src/fonts/font1.ttf");
+  src: url("../fonts/font1.ttf");
 }
 body{
   font-family: font1, sans-serif;
@@ -61,26 +71,43 @@ body{
   padding: 15px 100px;
   border-radius: 10px;
   z-index: 999;
+}
+.carte:hover{
   box-shadow: 10px 10px 45px #e1b12c;
+  cursor: pointer;
+  transition: all 0.5s ease-in-out;
 }
 h1{
   text-align: center;
   text-transform: uppercase;
 }
+p{
+  text-align: center;
+  font-size: 25px;
+}
+ul{
+  padding: 0;
+  text-align: center;
+}
 li{
   list-style: none  ;
-}
-.link-container{
-  display: flex;
+  text-align: center;
+  margin: 0 5px;
+  display: inline;
 }
 a{
   color: #7f8fa6  ;
   text-decoration: none;
   text-align: center;
   margin: auto;
+  border: 1px solid #7f8fa6;
+  padding: 5px 20px;
+  border-radius: 8px;
 }
 a:hover{
-  color: #fbc531;
+  border: none;
+  color: #353b48;
   transition: 0.5s all;
+  background: #e1b12c;
 }
 </style>
